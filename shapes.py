c24a2f76f803990d352f978c27fe78cd3446d395
bciereszynski <b.ciereszynski@wp.pl>
2023-10-06 02:07:40 +0200

    GK1 refactor shapes

--- shapes.py
+++ shapes.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import QPoint
 import math
+from abc import ABC, abstractmethod
 
 
 class Point:
@@ -35,17 +36,16 @@ class Point:
         return False
 
 
-class Line:
+# abstract
+class Shape(ABC):
     def __init__(self, point1, point2):
         self.point1 = point1
         self.point2 = point2
         self._resizePoint = point2
-        self._tolerance = 1
 
     def render(self, painter):
         self.point1.render(painter)
         self.point2.render(painter)
-        painter.drawLine(self.point1.x, self.point1.y, self.point2.x, self.point2.y)
 
     def resize(self, delta):
         if self._resizePoint is not None:
@@ -54,6 +54,34 @@ class Line:
     def move(self, delta):
         self.point1.move(delta)
         self.point2.move(delta)
+
+    @abstractmethod
+    def contains(self, position):
+        pass
+
+    def checkResize(self, position):
+        if self.point1.contains(position):
+            self._resizePoint = self.point1
+            return True
+        if self.point2.contains(position):
+            self._resizePoint = self.point2
+            return True
+        return False
+
+    def checkMove(self, position):
+        if self.contains(position) and not self.point2.contains(position):
+            return True
+        return False
+
+
+class Line(Shape):
+    def __init__(self, point1, point2):
+        super().__init__(point1, point2)
+        self._tolerance = 1
+
+    def render(self, painter):
+        super().render(painter)
+        painter.drawLine(self.point1.x, self.point1.y, self.point2.x, self.point2.y)
 
     def contains(self, position):
         distP1P2 = math.sqrt(math.pow(self.point1.x-self.point2.x, 2) + math.pow(self.point1.y - self.point2.y, 2))
@@ -65,41 +93,16 @@ class Line:
 
         return False
 
-    def checkResize(self, position):
-        if self.point1.contains(position):
-            self._resizePoint = self.point1
-            return True
-        if self.point2.contains(position):
-            self._resizePoint = self.point2
-            return True
-        return False
 
-    def checkMove(self, position):
-        if self.contains(position) and not self.point2.contains(position):
-            return True
-        return False
-
-
-class Circle:
+class Circle(Shape):
     def __init__(self, point1, point2):
+        super().__init__(point1, point2)
         self.r = None
-        self.point1 = point1
-        self.point2 = point2
-        self._resizePoint = point2
 
     def render(self, painter):
-        self.point1.render(painter)
-        self.point2.render(painter)
+        super().render(painter)
         self.r = math.sqrt(math.pow(self.point1.x - self.point2.x, 2) + math.pow(self.point1.y - self.point2.y, 2))
         painter.drawEllipse(QPoint(self.point1.x, self.point1.y), int(self.r), int(self.r))
-
-    def resize(self, delta):
-        if self._resizePoint is not None:
-            self._resizePoint.resize(delta)
-
-    def move(self, delta):
-        self.point1.move(delta)
-        self.point2.move(delta)
 
     def contains(self, position):
         if self.r is None:
@@ -109,36 +112,14 @@ class Circle:
             return True
         return False
 
-    def checkResize(self, position):
-        if self.point2.contains(position):
-            self._resizePoint = self.point2
-            return True
-        return False
 
-    def checkMove(self, position):
-        if self.contains(position) and not self.point2.contains(position):
-            return True
-        return False
-
-
-class Rectangle:
+class Rectangle(Shape):
     def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-        self._resizePoint = point2
+        super().__init__(point1, point2)
 
     def render(self, painter):
-        self.point1.render(painter)
-        self.point2.render(painter)
+        super().render(painter)
         painter.drawRect(self.point1.x, self.point1.y, self.point2.x-self.point1.x, self.point2.y-self.point1.y)
-
-    def resize(self, delta):
-        if self._resizePoint is not None:
-            self._resizePoint.resize(delta)
-
-    def move(self, delta):
-        self.point1.move(delta)
-        self.point2.move(delta)
 
     def contains(self, position):
         max_x = max(self.point1.x, self.point2.x)
@@ -146,19 +127,5 @@ class Rectangle:
         max_y = max(self.point1.y, self.point2.y)
         min_y = min(self.point1.y, self.point2.y)
         if min_x < position.x() < max_x and min_y < position.y() < max_y:
-            return True
-        return False
-
-    def checkResize(self, position):
-        if self.point1.contains(position):
-            self._resizePoint = self.point1
-            return True
-        if self.point2.contains(position):
-            self._resizePoint = self.point2
-            return True
-        return False
-
-    def checkMove(self, position):
-        if self.contains(position) and not self.point2.contains(position):
             return True
         return False
