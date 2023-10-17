2506f7c848fa107eeda2ebe2d1b8bdd7a2e661f9
bciereszynski <b.ciereszynski@wp.pl>
2023-10-17 19:08:15 +0200

    GK2 refactor code + add multithread exception handle

--- Files/FilesTab.py
+++ Files/FilesTab.py
@@ -1,15 +1,16 @@
 import threading
-from timeit import default_timer as timer
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QImage, qRgb, QPixmap
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QFileDialog
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QFileDialog, QMessageBox
 
-from Files.PpmHeader import PpmHeader
+from Files.PpmFile import PpmFile
 from Files.SyntaxException import SyntaxException
 
 
 class FilesTab(QWidget):
+    exceptSignal = pyqtSignal(str)
+
     def __init__(self, parent=None):
         super(FilesTab, self).__init__(parent)
 
@@ -26,125 +27,38 @@ class FilesTab(QWidget):
         self.lay.addWidget(self.label)
         self.lay.setAlignment(self.label, Qt.AlignCenter)
 
+        self.exceptSignal.connect(self.__showExceptionMessage)
+
     def loadBtnCommand(self):
-        thread = threading.Thread(target=self.loadPPM, args=())
+        thread = threading.Thread(target=self.loadFile, args=())
         thread.start()
 
-    def loadPPM(self):
-
+    def loadFile(self):
         fileName = QFileDialog.getOpenFileName(self)
-        start = timer()
         if not fileName[0]:
             return
 
-        with open(fileName[0], "rb") as file:
-            header, currentLine = self.__readFileHeader(file)
-            lines = file.readlines()
-
-        image = QImage(header.columns, header.rows, QImage.Format_RGB32)
-        if header.format == "P3":
-            # continue where header ends
-            words = currentLine.split()
-            words = words[words.index(str(header.colorScale).encode()) + 1:]
-            lines.insert(0, ' '.join(words).encode())
-            self.__readP3FileData(lines, header, image)
-        elif header.format == "P6":
-            self.__readP6FileData(lines, header, image)
+        if fileName[0].find(".ppm") != -1:
+            file = PpmFile()
+            try:
+                image = file.load(fileName[0])
+            except SyntaxException as ex:
+                self.exceptSignal.emit(ex.message)
+                return
         else:
-            raise SyntaxException("Unknown format")
+            self.exceptSignal.emit("File format is not supported - " + fileName[0])
+            return
 
-        end = timer()
-        print(end - start)
         image = image.scaledToWidth(1200)
         image = image.scaledToHeight(600)
         pixmap = QPixmap().fromImage(image)
         self.label.setPixmap(pixmap)
 
     @staticmethod
-    def __readP6FileData(lines, header, image):
-        rgb = []
-        currentRow = 0
-        currentCol = 0
-
-        if header.colorScale != 255:
-            raise SyntaxException("Invalid color scale")
-
-        for line in lines:
-            for number in line:
-                rgb.append(number)
-                if len(rgb) == 3:
-                    if currentCol >= header.columns:
-                        currentCol = 0
-                        currentRow += 1
-                    value = qRgb(rgb[0], rgb[1], rgb[2])
-                    image.setPixel(currentCol, currentRow, value)
-                    currentCol += 1
-                    rgb = []
-        if rgb:
-            raise SyntaxException("Data not match header - rgb error")
-        if currentRow != header.rows - 1 or currentCol != header.columns:
-            raise SyntaxException("Data not match header - pixels error")
-
-    @staticmethod
-    def __readP3FileData(lines, header, image):
-        rgb = []
-        currentRow = 0
-        currentCol = 0
-        for line in lines:
-            words = line.split()
-            for word in words:
-                try:
-                    if chr(word[0]) == "#":
-                        break
-                    number = int(word.decode())
-                except Exception:
-                    raise SyntaxException("Not integer passed")
-
-                rgb.append(number)
-                if len(rgb) == 3:
-                    if currentCol >= header.columns:
-                        currentCol = 0
-                        currentRow += 1
-                    value = qRgb(rgb[0], rgb[1], rgb[2])
-                    image.setPixel(currentCol, currentRow, value)
-                    currentCol += 1
-                    rgb = []
-
-        if rgb:
-            raise SyntaxException("Data not match header - rgb error")
-        if currentRow != header.rows - 1 or currentCol != header.columns:
-            raise SyntaxException("Data not match header - pixels error")
-
-    @staticmethod
-    def __readFileHeader(file):
-        header = PpmHeader()
-        line = file.readline()
-        while line:
-            for word in line.split():
-                word = word.decode()
-                if word[0] == "#":
-                    break
-                if header.format is None:
-                    header.format = word
-                    if header.format != "P3" and header.format != "P6":
-                        raise SyntaxException("Bad format - " + header.format)
-                elif header.columns is None:
-                    try:
-                        header.columns = int(word)
-                    except Exception:
-                        raise SyntaxException("Integer expected - " + word)
-                elif header.rows is None:
-                    try:
-                        header.rows = int(word)
-                    except Exception:
-                        raise SyntaxException("Integer expected - " + word)
-                elif header.colorScale is None:
-                    try:
-                        header.colorScale = int(word)
-                    except Exception:
-                        raise SyntaxException("Integer expected - " + word)
-                    if not 0 < header.colorScale < 65536:
-                        raise SyntaxException("Invalid color scale")
-                    return header, line
-            line = file.readline()
-        raise SyntaxException("Header incomplete")
+    def __showExceptionMessage(msg):
+        messageBox = QMessageBox()
+        messageBox.setText("Error occurred")
+        messageBox.setInformativeText(msg)
+        messageBox.setIcon(QMessageBox.Warning)
+        messageBox.setWindowTitle("Error")
+        messageBox.exec()
---
+++ Files/PpmFile.py
@@ -0,0 +1,111 @@
+from PyQt5.QtGui import QImage, qRgb
+from PyQt5.QtWidgets import QFileDialog
+from timeit import default_timer as timer
+
+from Files.PpmHeader import PpmHeader
+from Files.SyntaxException import SyntaxException
+
+
+class PpmFile:
+
+    def __init__(self):
+        self.header = PpmHeader()
+        self.currentRgb = []
+        self.currentRow = 0
+        self.currentCol = 0
+        self.image = None
+
+    def load(self, fileName):
+        with open(fileName, "rb") as file:
+            currentLine = self.__readFileHeader(file)
+            lines = file.readlines()
+
+        start = timer()
+        self.image = QImage(self.header.columns, self.header.rows, QImage.Format_RGB32)
+
+        # continue where header ends
+        words = currentLine.split()
+        words = words[words.index(str(self.header.colorScale).encode()) + 1:]
+        lines.insert(0, ' '.join(words).encode())
+
+        if self.header.format == "P3":
+            self.__readP3FileData(lines)
+        elif self.header.format == "P6":
+            self.__readP6FileData(lines)
+        else:
+            raise SyntaxException("Unknown format")
+
+        end = timer()
+        print(end - start)
+
+        if self.currentRgb:
+            raise SyntaxException("Data not match header - rgb error")
+        if self.currentRow != self.header.rows - 1 or self.currentCol != self.header.columns:
+            raise SyntaxException("Data not match header - pixels error")
+        return self.image
+
+    def __readFileHeader(self, file):
+        line = file.readline()
+        while line:
+            for word in line.split():
+                try:
+                    if chr(word[0]) == "#":
+                        break
+                    word = word.decode()
+                except UnicodeError:
+                    raise SyntaxException("Not ascii symbol in header")
+
+                if self.header.format is None:
+                    self.header.format = word
+                    if self.header.format != "P3" and self.header.format != "P6":
+                        raise SyntaxException("Bad format - " + self.header.format)
+                    continue
+
+                try:
+                    number = int(word)
+                except Exception:
+                    raise SyntaxException("Integer expected - " + word)
+
+                if self.header.columns is None:
+                    self.header.columns = number
+                elif self.header.rows is None:
+                    self.header.rows = number
+                elif self.header.colorScale is None:
+                    self.header.colorScale = number
+                    if not 0 < self.header.colorScale < 65536:
+                        raise SyntaxException("Invalid color scale")
+                    return line
+            line = file.readline()
+        raise SyntaxException("Header incomplete")
+
+    def addRgb(self, number):
+        self.currentRgb.append(number)
+        if len(self.currentRgb) == 3:
+            if self.currentCol >= self.header.columns:
+                self.currentCol = 0
+                self.currentRow += 1
+            value = qRgb(self.currentRgb[0], self.currentRgb[1], self.currentRgb[2])
+            self.image.setPixel(self.currentCol, self.currentRow, value)
+            self.currentCol += 1
+            self.currentRgb = []
+
+    def __readP6FileData(self, lines):
+        if self.header.colorScale != 255:
+            raise SyntaxException("Invalid color scale")
+
+        for line in lines:
+            for number in line:
+                self.addRgb(number)
+
+    def __readP3FileData(self, lines):
+        for line in lines:
+            words = line.split()
+            for word in words:
+                try:
+                    if chr(word[0]) == "#":
+                        break
+                    number = int(word.decode())
+                except UnicodeError:
+                    raise SyntaxException("Not integer passed")
+
+                self.addRgb(number)
