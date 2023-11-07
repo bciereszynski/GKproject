0ada5648ae91a80b927438279f1c6bd6a1f55b82
bciereszynski <b.ciereszynski@wp.pl>
2023-11-07 21:02:52 +0100

    GK4 add masks handling

--- ImageOperations/ImageEditor.py
+++ ImageOperations/ImageEditor.py
@@ -48,6 +48,8 @@ class ImageEditor(QWidget):
         self.controlsLay.addLayout(self.comboLay)
         self.controlsLay.setAlignment(self.comboLay, Qt.AlignTop)
 
+        self.controlsLay.addSpacing(20)
+
         self.spin = QSpinBox()
         self.spin.setMaximum(255)
         self.spin.setMinimum(0)
@@ -59,16 +61,36 @@ class ImageEditor(QWidget):
 
         self.controlsLay.addSpacing(20)
 
+        self.__createControlButton("Average", self.setAvgMask)
+        self.__createControlButton("Sobel", self.setSobel)
+        self.__createControlButton("Sharp", self.setSharp)
+        self.__createControlButton("Gauss", self.setGauss)
+
         self.maskEdit = QPlainTextEdit()
         self.maskEdit.setFixedSize(61, 100)
         self.maskEdit.setLineWrapMode(QPlainTextEdit.NoWrap)
         self.controlsLay.addWidget(self.maskEdit)
 
-        self.applyMaskBtn = QPushButton("Apply mask")
-        self.applyMaskBtn.clicked.connect(self.applyMask)
-        self.controlsLay.addWidget(self.applyMaskBtn)
+        self.__createControlButton("Median", self.medianFilter)
+        self.__createControlButton("Apply mask", self.applyMask)
 
         self.lay.addLayout(self.controlsLay, stretch=1)
+
+    def setAvgMask(self):
+        text = "1 1 1\n1 1 1\n1 1 1"
+        self.maskEdit.setPlainText(text)
+
+    def setSobel(self):
+        text = "1 0 -1\n2 0 -2\n1 0 -1"
+        self.maskEdit.setPlainText(text)
+
+    def setSharp(self):
+        text = "0 -1 0\n-1 5 -1\n0 -1 0"
+        self.maskEdit.setPlainText(text)
+
+    def setGauss(self):
+        text = "1 4 6 4 1\n4 16 24 16 4\n6 24 36 24 6\n4 16 24 16 4\n1 4 6 4 1"
+        self.maskEdit.setPlainText(text)
 
     def mouseMoveEvent(self, e):
         super()
@@ -191,6 +213,34 @@ class ImageEditor(QWidget):
             return
 
         image.save(fileName[0], "jpeg", 100 - value)
+
+    def medianFilter(self):
+        channels = [self.redCheckCombo.isChecked(), self.greenCheckCombo.isChecked(),
+                          self.blueCheckCombo.isChecked()]
+        image = QImage(self.currentImage)
+        mask_size = 3
+        reach = mask_size // 2
+        for x in range(reach, image.width() - reach):
+            for y in range(reach, image.height() - reach):
+                rList = []
+                gList = []
+                bList = []
+                for i in range(mask_size):
+                    for j in range(mask_size):
+                        oldRgb = self.currentImage.pixel(x + i - reach, y + j - reach)
+                        rList.append(qRed(oldRgb))
+                        gList.append(qGreen(oldRgb))
+                        bList.append(qBlue(oldRgb))
+                rList.sort()
+                gList.sort()
+                bList.sort()
+                rgb = self.currentImage.pixel(x, y)
+                r = qRed(rgb) if not channels[0] else rList[4]
+                g = qGreen(rgb) if not channels[1] else gList[4]
+                b = qBlue(rgb) if not channels[2] else bList[4]
+                newRgb = qRgb(r, g, b)
+                image.setPixel(x, y, newRgb)
+        self.updateImage(image)
 
     def __maskImage(self, mask: list[list], channels):
         image = QImage(self.currentImage)
