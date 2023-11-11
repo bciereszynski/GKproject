38d6b3228cad1728af9a058a2cd8d381d83818fa
bciereszynski <b.ciereszynski@wp.pl>
2023-11-11 19:55:24 +0100

    GK5 add binarization

--- HistogramAndBinarization/HistogramEditor.py
+++ HistogramAndBinarization/HistogramEditor.py
@@ -1,5 +1,6 @@
 from PyQt5.QtGui import QImage, qRed, qRgb
 import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import QInputDialog
 
 from ImageOperations.ImageEditor import ImageEditor
 
@@ -9,6 +10,10 @@ class HistogramEditor(ImageEditor):
         super().__init__(parent)
         self.createControlButton("Stretch", self.stretch)
         self.createControlButton("Equalize", self.equalize)
+
+        self.controlsLay.addSpacing(20)
+        self.createControlButton("Binarize with value", self.binarizeWithValue)
+        self.createControlButton("Binarize with percent", self.binarizeWithPercent)
 
     def createHistogram(self):
         histogram = [0] * 256
@@ -41,6 +46,7 @@ class HistogramEditor(ImageEditor):
                 newValue = int(newValue*255)
                 image.setPixel(x, y, qRgb(newValue, newValue, newValue))
         self.updateImage(image)
+
     def stretch(self):
         self.toGrayscaleLinear()
         image = QImage(self.currentImage)
@@ -58,5 +64,42 @@ class HistogramEditor(ImageEditor):
             for y in range(image.height()):
                 value = qRed(image.pixel(x, y))
                 newValue = int((value - valueMin) / valueDiff * 255)
+                image.setPixel(x, y, qRgb(newValue, newValue, newValue))
+        self.updateImage(image)
+
+    def binarizeWithValue(self):
+        (value, ok) = QInputDialog().getInt(self, "Threshold", "Threshold value:", 100, 0, 255)
+        if not ok:
+            return
+        self.binarize(value)
+
+    def binarizeWithPercent(self):
+        (percent, ok) = QInputDialog().getInt(self, "Threshold", "Threshold percent:", 50, 0, 100)
+        if not ok:
+            return
+
+        histogram = self.createHistogram()
+        desiredValue = sum(histogram) * (percent/100)
+        threshold = -1
+        accum = 0
+
+        while accum < desiredValue:
+            threshold += 1
+            accum += histogram[threshold]
+
+        self.binarize(threshold)
+
+    def binarize(self, threshold):
+        self.toGrayscaleLinear()
+        image = QImage(self.currentImage)
+
+        LUT = [0] * 256
+        for i in range(256):
+            LUT[i] = 0 if i < threshold else 255
+
+        for x in range(image.width()):
+            for y in range(image.height()):
+                value = qRed(image.pixel(x, y))
+                newValue = LUT[value]
                 image.setPixel(x, y, qRgb(newValue, newValue, newValue))
         self.updateImage(image)
